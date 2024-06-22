0e1e2d6b4fe2f35e9d3cc54b7d9d68b8f53f4798
Luca Becker <luca.becker@me.com>
2024-06-22 15:09:45 +0200

    refactor: Update rollup configuration and package.json
    * Update build script to use rollup.config.ts file
    * Add kolorist and @rollup/plugin-json dependencies
    * Update rollup.config.ts to use src/cli.ts as input and output to dist/cli.mjs in esm format
    * Remove unused dependencies and configurations in rollup.config.ts

--- rollup.config.ts
+++ rollup.config.ts
@@ -2,21 +2,18 @@ import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import typescript from '@rollup/plugin-typescript';
 import terser from '@rollup/plugin-terser';
+import json from '@rollup/plugin-json';
+import type { RollupOptions } from 'rollup';
 
 export default {
-    input: 'src/index.ts',
+    input: 'src/cli.ts',
     output: {
-        file: 'dist/bundle.js',
-        format: 'cjs',
+        file: 'dist/cli.mjs',
+        format: 'esm',
         sourcemap: true,
     },
-    plugins: [
-        resolve(),
-        commonjs(),
-        typescript(), // This will use the tsconfig.json file by default
-        terser(),
-    ],
+    plugins: [resolve(), commonjs(), typescript(), json(), terser()],
     external: [
         // List external dependencies here
     ],
-};
+} satisfies RollupOptions;
