e2332c27d890007c517453c1ec801bdad163f3d5
Luca Becker <luca.becker@me.com>
2025-04-22 20:08:35 +0200

    refactor: update eslint config
    * updating eslint configuration to ignore dist directory recursively
    * adding curly rule to enforce consistent brace style for all control structures

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,15 @@
 import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
 
-export default tseslint.config(eslint.configs.recommended, ...tseslint.configs.recommended, {
-    ignores: ['dist'],
-});
+export default tseslint.config(
+    {
+        ignores: ['dist/**'],
+    },
+    eslint.configs.recommended,
+    ...tseslint.configs.recommended,
+    {
+        rules: {
+            curly: ['error', 'all'],
+        },
+    },
+);
